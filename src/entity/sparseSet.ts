85496148c7252e80b1ba4221b086db460f449678
George Turkin <fxlr8@mail.ru>
2019-04-23 19:01:41 +0300

    basic implementation of sparse set

---
+++ src/entity/sparseSet.ts
@@ -0,0 +1,66 @@
+function assert(condition: boolean, message: string): void {
+		if (!condition) {
+				message = message || 'Assertion failed'
+				if (typeof Error !== 'undefined') {
+						throw new Error(message)
+				}
+				throw message // Fallback
+		}
+}
+
+function swap(arr: any[], lhs: number, rhs: number) {
+	const tmp = arr[lhs]
+	arr[lhs] = arr[rhs]
+	arr[rhs] = tmp
+}
+
+/**
+ * Represents a sparse set
+ */
+export default class SparseSet {
+	private direct: number[] = []
+	private reverse: number[] = []
+
+	public has(entity: number): boolean {
+		return entity < this.reverse.length && this.reverse[entity] !== undefined
+	}
+
+	public construct(entity: number): void {
+		assert(!this.has(entity), 'Entity already exists')
+		this.reverse[entity] = this.direct.length
+		this.direct.push(entity)
+	}
+
+	public swap(lhs: number, rhs: number): void {
+		assert(lhs < this.direct.length, 'First entity out of range')
+		assert(rhs < this.direct.length, 'Second entity out of range')
+
+		swap(this.direct, lhs, rhs)
+		swap(this.reverse, this.direct[lhs], this.direct[rhs])
+	}
+
+	public destroy(entity: number): void {
+		assert(this.has(entity), 'Entity to delete is missing')
+		const candidate = this.reverse[entity]
+		this.direct[candidate] = this.direct[this.direct.length - 1]
+		this.reverse[this.direct[this.direct.length - 1]] = candidate
+		this.direct.pop()
+		delete this.reverse[entity]
+	}
+
+	public get(entity: number) {
+		return this.reverse[entity]
+	}
+
+	public get empty() {
+		return this.direct.length === 0
+	}
+
+	public get size() {
+		return this.direct.length
+	}
+
+	public get data() {
+		return this.direct
+	}
+}
